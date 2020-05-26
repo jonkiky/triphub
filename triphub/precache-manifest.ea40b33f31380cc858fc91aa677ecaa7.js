c9c7bab50a50652c2f323d7b47448fbcf805e969
Yizhen Chen <jonkiky@Yizhens-MacBook-Pro.local>
2020-05-25 21:22:46 -0400

    Updates

---
+++ triphub/precache-manifest.ea40b33f31380cc858fc91aa677ecaa7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f810148a10cbf715214c16ccc21e63a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4145f2c5b11421b0957c",
+    "url": "./static/css/main.d667c1c6.chunk.css"
+  },
+  {
+    "revision": "1c032cf418a825a36e2c",
+    "url": "./static/js/2.e0091d18.chunk.js"
+  },
+  {
+    "revision": "6cc76ac6f57a2f448676ac03d83c6d15",
+    "url": "./static/js/2.e0091d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4145f2c5b11421b0957c",
+    "url": "./static/js/main.354f4879.chunk.js"
+  },
+  {
+    "revision": "4ce73f8449f643763ba5",
+    "url": "./static/js/runtime-main.6f4f5fb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "defc129761cd0670ef2a278ffa1c5305",
+    "url": "./static/media/not-found-image.defc1297.jpg"
+  }
+]);
