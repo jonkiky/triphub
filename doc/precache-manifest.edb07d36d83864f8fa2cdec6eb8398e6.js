2603dcf14c88ea7b7a4503ca6cdc69861eca665e
Chen <jonkiky@gmail.com>
2021-06-04 01:14:57 -0400

    add prod code

---
+++ doc/precache-manifest.edb07d36d83864f8fa2cdec6eb8398e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9d263c00e2e00519d2654cf375ac24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0dde31800045354f546",
+    "url": "./static/css/2.ad28d71b.chunk.css"
+  },
+  {
+    "revision": "31bacdbff3a4bd035b58",
+    "url": "./static/css/main.c6a7c8e3.chunk.css"
+  },
+  {
+    "revision": "a0dde31800045354f546",
+    "url": "./static/js/2.306463bf.chunk.js"
+  },
+  {
+    "revision": "b87c73aef5272e673958f219c7db0a32",
+    "url": "./static/js/2.306463bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bacdbff3a4bd035b58",
+    "url": "./static/js/main.8e67151a.chunk.js"
+  },
+  {
+    "revision": "204507d50144289a9267",
+    "url": "./static/js/runtime-main.7a5c02b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "defc129761cd0670ef2a278ffa1c5305",
+    "url": "./static/media/not-found-image.defc1297.jpg"
+  }
+]);
